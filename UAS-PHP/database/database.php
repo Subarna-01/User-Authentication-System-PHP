74b5a64373c3b766791a0748c33e54d7a279442d
Subarna Bhowmik <104106066+Subarna-01@users.noreply.github.com>
2023-05-21 02:45:55 +0530

    Update database.php

--- UAS-PHP/database/database.php
+++ UAS-PHP/database/database.php
@@ -1,8 +1,8 @@
 <?php
     $servername = "localhost";
     $username = "root";
-    $password = "sub@2001";
-    $dbname = "test_app";
+    $password = "";
+    $dbname = "";
 
     $conn = mysqli_connect($servername, $username, $password,$dbname);
 
